c1c1408385cfb254fd9911e291ad2fc590f74d43
KrillM <mks160615@gmail.com>
2023-12-12 14:21:48 +0900

    redux beginning

---
+++ 07_redux/src/AppNew.js
@@ -0,0 +1,11 @@
+// import { useState } from "react";
+import "./App.css";
+
+function AppNew() {
+  return (
+    <>
+    </>
+  );
+}
+
+export default AppNew;
---
+++ 07_redux/src/index.js
@@ -0,0 +1,42 @@
+// AppOld
+// import React from 'react';
+// import ReactDOM from 'react-dom/client';
+// import './index.css';
+// import AppOld from './AppOld';
+
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(
+//   <AppOld />
+// );
+
+// AppNew 4 Redux
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import AppNew from './AppNew';
+
+// 추가된 부분
+import { Provider } from "react-redux";
+import { configureStore } from "@reduxjs/toolkit";
+
+const initialValue = {number: 100};
+
+const reducer = (state = initialValue, action) => {
+  switch(action.type) {
+    case 'increment':
+      return {number: state.number + 1};
+    case 'decrement':
+      return {number: state.number - 1};
+    default:
+      return state;
+  }
+}
+
+const store = configureStore({reducer: reducer})
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <Provider store={store}>
+    <AppNew />
+  </Provider>
+);
